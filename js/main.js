7b5a7be24d372c682eebd4d4b5d099d9aced33fa
kamcha7ka <sledsib@ya.ru>
2020-08-06 14:30:00 +0300

    Добавит код для генерации и отображения тестовых данных на карте.

---
+++ js/main.js
@@ -0,0 +1,106 @@
+'use strict';
+
+let getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let getMockData = function (lengthArray, mapWidth) {
+
+  let TemplateString = {
+    AVATAR_URL: 'img/avatars/user0',
+    AVATAR_TYPE: '.png',
+    PHOTO_URL: 'http://o0.github.io/assets/images/tokyo/hotel',
+    PHOTO_TYPE: '.jpg',
+    TYPE: ['palace', 'flat', 'house', 'bungalo'],
+    TIME: ['12:00', '13:00', '14:00'],
+    FEATURE: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+  };
+
+  let data = [];
+
+  let getArrayPhoto = function (lengthArray) {
+    let array = [];
+    for (let i = 0; i < lengthArray; i++) {
+      array.push(TemplateString.PHOTO_URL + i + TemplateString.PHOTO_TYPE);
+    }
+    return array;
+  };
+
+  let getArrayFeature = function (lengthArray) {
+    let array = [];
+    for (let i = 0; i < lengthArray; i++) {
+      array.push(TemplateString.FEATURE[i]);
+    }
+    return array;
+  };
+
+  for (let i = 1; i <= lengthArray; i++) {
+    data.push({
+      avatar: TemplateString.AVATAR_URL + i + TemplateString.AVATAR_TYPE,
+      offer: {
+        title: 'Title #' + i,
+        price: getRandomNumber(500, 50000),
+        type: TemplateString.TYPE[getRandomNumber(0, TemplateString.TYPE.length - 1)],
+        rooms: getRandomNumber(1, 5),
+        guests: getRandomNumber(1, 5),
+        checkin: TemplateString.TIME[getRandomNumber(0, TemplateString.TIME.length - 1)],
+        checkout: TemplateString.TIME[getRandomNumber(0, TemplateString.TIME.length - 1)],
+        features: getArrayFeature(getRandomNumber(1, TemplateString.FEATURE.length - 1)),
+        description: 'Description #' + i,
+        photos: getArrayPhoto(getRandomNumber(1, 8))
+      },
+      location: {
+        x: getRandomNumber(0, mapWidth),
+        y: getRandomNumber(130, 630)
+      }
+    });
+
+    data[data.length - 1].offer.address = data[data.length - 1].location.x + ', ' + data[data.length - 1].location.y;
+  };
+
+  return data;
+};
+
+let generatePins = function (data) {
+  let Pin = {
+    TEMPLATE_ID: '#pin',
+    ELEMENT_CLASS: '.map__pin',
+    WIDTH: 50,
+    HEIGHT: 70
+  }
+
+  let fragment = document.createDocumentFragment();
+  let nodeTemplate = document.querySelector(Pin.TEMPLATE_ID).content;
+
+  for (let i = 0; i < data.length; i++) {
+    let node = nodeTemplate.cloneNode(true);
+    let pinElement = node.querySelector(Pin.ELEMENT_CLASS);
+    pinElement.style.left = data[i].location.x - (Pin.WIDTH / 2) + 'px';
+    pinElement.style.top = data[i].location.y - Pin.HEIGHT + 'px';
+    let imgElement = pinElement.querySelector('img');
+    imgElement.src = data[i].avatar;
+    imgElement.alt = data[i].offer.title;
+    fragment.appendChild(node);
+  }
+
+  return fragment;
+};
+
+let init = function () {
+
+  let MOCKDATA_LENGTH = 8;
+  
+  let Element = {
+    MAP_CLASS: '.map',
+    MAP_CLASS_FADED: 'map--faded'
+  };
+
+  let mapElement = document.querySelector(Element.MAP_CLASS);
+  mapElement.classList.remove(Element.MAP_CLASS_FADED);
+
+  let mapElementWidth = mapElement.offsetWidth;
+
+  mapElement.appendChild(generatePins(getMockData(MOCKDATA_LENGTH, mapElementWidth)));
+};
+
+window.addEventListener('load', init);
